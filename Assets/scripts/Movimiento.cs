0704408bba00b41784129b370961a40a471201ad
Armando Terrazas Gómez <mandotrs@icloud.com>
2023-02-17 08:55:37 -0600

    Primer commit

---
+++ Assets/scripts/Movimiento.cs
@@ -0,0 +1,35 @@
+// estamos usando .NET
+// aquí importamos namespaces
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimiento : MonoBehaviour
+{
+    //Ciclo de vida / Life Cycle
+    // Existen métodos que se ejecutan en ciertos momentos del ciclo de vida
+
+    void awake() {
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate() {
+
+    }
+
+    void LateUpdate() {
+
+    }
+}
